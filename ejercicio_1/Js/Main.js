f1cd8986368ab042ff5bcc95d3bf0589aadca8ea
Jhonatan Albadan <tatan19972008@hotmail.com>
2023-05-28 20:05:28 -0500

    Primer avance ejercicios js

---
+++ ejercicio_1/Js/Main.js
@@ -0,0 +1,94 @@
+/*Pregunta 1*/ 
+/*    let bella = prompt(`Responde con un "si" o un "no"¿Eres bellisimo/a?.`);
+
+        if (bella=="si"){
+            console.log(`Ciertamente`);
+        }else if (bella=="no"){
+            console.log(`No te creo`)
+        }else{
+            console.log(`Opcion Errada`);
+        }
+
+
+/*Pregunta 2*/ 
+/*   let numero = prompt("Ingrese un número: ");
+
+        if (numero % 2 === 0) {
+        console.log(numero + " es divisible por 2");
+        } else {
+        console.log(numero + " no es divisible por 2");
+        }
+
+
+/*Pregunta 3*/ 
+/*    let numero = prompt("Ingrese un número: ");
+
+        if (numero % 2 === 0) {
+        alert(numero + " Es Par");
+        } else {
+        alert(numero + " Es Inpar");
+        }
+
+
+/*Pregunta 4*/
+/*    let numero = prompt("Ingrese un número boleto: ");
+
+        if (numero == 1000) {
+        console.log("Ganaste un premio");
+        } else {
+        console.log("Lo sentimos, sigue participando");
+        }
+
+
+/*Pregunta 5*/ 
+/*    let numero1 = prompt("Ingrese el primer numero: ");
+    let numero2 = prompt("Ingrese el segundo numero:"):
+        if (numero1 > numero2) {
+            console.log(`El ${numero2} es menor que ${numero1}`);
+        } else if (numero1 < numero2){
+            console.log(`El ${numero1} es menor que ${numero2}`);
+        }else{
+            console.log(`los numeros son iguales`)
+        }
+
+/*pregunta 6*/ 
+    let numero1 = prompt("Ingrese el primer numero: ");
+    let numero2 = prompt("Ingrese el segundo numero: ");
+    let numero3 = prompt("Ingrese el tercer numero: ");
+       
+    if(numero1 == numero2 && numero1 > numero3){
+            console.log(`El ${numero1} y el numero ${numero2} son iguales y mayores que el ${numero3}`);
+        }else if(numero2 == numero3 && numero2 > numero1){
+            console.log(`El ${numero2} y el numero ${numero3} son iguales y mayores que el ${numero1}`);
+        }else if(numero3 == numero1 && numero3 > numero2){
+            console.log(`El ${numero3} y el numero ${numero1} son iguales y mayores que el ${numero2}`);
+        }else if (numero2 > numero1 && numero2 > numero3){
+            console.log(`El ${numero2} es mayor que ${numero1} y ${numero3}`);
+        }else if(numero3 > numero1 && numero3 > numero2){
+            console.log(`El ${numero3} es mayor que ${numero1} y ${numero2}`);
+        }else if (numero1 > numero2 && numero1 > numero3) {
+            console.log(`El ${numero1} es mayor que ${numero2} y ${numero3}`);
+        }
+
+
+/*Variba*/ 
+
+/*Variba*/ 
+
+/*Variba*/ 
+
+/*Variba*/ 
+
+/*Variba*/ 
+
+
+
+/*let nombre = "";
+let apellido = "";
+
+
+nombre = prompt("ingresa tu nombre");
+console.log(`El nombre ingresado es: ${nombre}.`);
+
+apellido = prompt("ingresa tu apellido");
+console.log(`El apellido ingresado es: ${apellido}.`);*/
