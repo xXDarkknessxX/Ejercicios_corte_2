3277a7108daec79ec70f4f2bf02f9676b1a3af45
Jhonatan Albadan <tatan19972008@hotmail.com>
2023-06-12 14:52:48 -0500

    finalizacion ejercicio 4

--- ejercicio_4/js/main.js
+++ ejercicio_4/js/main.js
@@ -7,7 +7,7 @@
   }
 
 /*ejercicio 2 */
-/*let pokemons = [
+/*const pokemons = [
     'bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard',
     'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree',
     'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata',
@@ -33,4 +33,10 @@
     }
   }
   /*ejercicio 3*/
-  
+const arreglo = [4,"dos",8,"tres",5,9,1,"cero"];
+
+for(let i = 0; i <= arreglo.length; i++){
+if (typeof arreglo [i] === 'number'){
+    console.log(arreglo[i]+ ` es un numero`)
+}
+} 
