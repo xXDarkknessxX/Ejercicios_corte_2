2bba7bef4f60cfef324c04ace5681ba7521ad735
Jhonatan Albadan <tatan19972008@hotmail.com>
2023-06-12 14:47:27 -0500

    correccion

---
+++ ejercicio_4/js/main.js
@@ -0,0 +1,36 @@
+/*ejercicio 1 */
+
+/*for (let i = 1; i <= 50; i++) { 
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
+
+/*ejercicio 2 */
+/*let pokemons = [
+    'bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard',
+    'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree',
+    'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata',
+    'raticate', 'spearow', 'fearow', 'ekans', 'arbok', 'pikachu', 'raichu',
+    'sandshrew', 'sandslash', 'nidoran-f', 'nidorina', 'nidoqueen', 'nidoran-m',
+    'nidorino', 'nidoking', 'clefairy', 'clefable', 'vulpix', 'ninetales',
+    'jigglypuff', 'wigglytuff', 'zubat', 'golbat', 'oddish', 'gloom', 'vileplume',
+    'paras', 'parasect', 'venonat', 'venomoth', 'diglett', 'dugtrio', 'meowth',
+    'persian', 'psyduck', 'golduck', 'mankey', 'primeape', 'growlithe', 'arcanine',
+    'poliwag', 'poliwhirl', 'poliwrath', 'abra', 'kadabra', 'alakazam', 'machop',
+    'machoke', 'machamp', 'bellsprout', 'weepinbell', 'victreebel', 'tentacool',
+    'tentacruel', 'geodude', 'graveler', 'golem', 'ponyta', 'rapidash', 'slowpoke',
+    'slowbro', 'magnemite', 'magneton', 'farfetchd', 'doduo', 'dodrio', 'seel',
+    'dewgong', 'grimer', 'muk', 'shellder', 'cloyster', 'gastly', 'haunter',
+    'gengar', 'onix', 'drowzee', 'hypno', 'krabby', 'kingler', 'voltorb'
+  ];
+  
+  let numeroUsuario = parseInt(prompt("Introduce un número:"));
+
+  for (let i = 1; i <= numeroUsuario; i++) {
+    if (i % 5 === 0) {
+      console.log(pokemons[i - 1]);
+    }
+  }
+  /*ejercicio 3*/
+  
