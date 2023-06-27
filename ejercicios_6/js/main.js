ced463916b59a5accf0196dc7501606f7c082e91
Jhonatan Albadan <tatan19972008@hotmail.com>
2023-06-27 14:01:06 -0500

    entrega de trabajos

--- ejercicios_6/js/main.js
+++ ejercicios_6/js/main.js
@@ -1,24 +1,72 @@
-const juanaArreglo = [];
-const pedroArreglo = [];
-
-let aqua = 200;
-let emocion = 180;
-let alegria = 160;
-let frescura = 150;
-let formJuana = document.getElementById(`formJuana`);
-
-/*formJuana.addEventListener(`submit`, juana);
-function juana(event){
-    event.preventDefault();
-    
-    if(document.getElementById(`formJuana`)!==0){
-        console.log(juana)
+var ventas = {
+    Juana: { Aqua: 0, Emoción: 0, Alegría: 0, Frescura: 0 },
+    Pedro: { Aqua: 0, Emoción: 0, Alegría: 0, Frescura: 0 }
+  };
+  
+  function registrarVenta() {
+    var vendedor = document.getElementById('vendedor').value;
+    var producto = document.getElementById('producto').value;
+    var cantidad = parseInt(document.getElementById('cantidad').value);
+  
+    if (!isNaN(cantidad)) {
+      ventas[vendedor][producto] += cantidad;
+      console.log('Venta registrada:', vendedor, '-', producto, '-', cantidad);
+    } else {
+      console.error('Error: La cantidad debe ser un valor numérico.');
     }
-
-}
-console.log(juana)*/
-
-formJuana.addEventListener("click", function(){
-
-    
-});
+  }
+  
+  function calcularResultados() {
+    var totalVentas = {
+      Juana: 0,
+      Pedro: 0
+    };
+  
+    for (var vendedor in ventas) {
+      for (var producto in ventas[vendedor]) {
+        var cantidad = ventas[vendedor][producto];
+        var precio = obtenerPrecio(producto);
+        var total = cantidad * precio;
+        totalVentas[vendedor] += total;
+      }
+    }
+  
+    for (var vendedor in ventas) {
+      console.log('---', vendedor, '---');
+      for (var producto in ventas[vendedor]) {
+        var cantidad = ventas[vendedor][producto];
+        console.log('Producto:', producto, '- Cantidad:', cantidad);
+      }
+    }
+  
+    var vendedorEmpleadoMes = '';
+    var maxVentas = 0;
+    var empate = false;
+    for (var vendedor in totalVentas) {
+      var ventasVendedor = totalVentas[vendedor];
+      if (ventasVendedor > maxVentas) {
+        vendedorEmpleadoMes = vendedor;
+        maxVentas = ventasVendedor;
+        empate = false;
+      } else if (ventasVendedor === maxVentas) {
+        empate = true;
+      }
+    }
+  
+    if (empate) {
+      console.log('No se puede determinar un empleado del mes debido a un empate.');
+    } else {
+      console.log('Empleado del Mes:', vendedorEmpleadoMes);
+    }
+  }
+  
+  function obtenerPrecio(producto) {
+    var precios = {
+      Aqua: 200,
+      Emoción: 180,
+      Alegría: 160,
+      Frescura: 150
+    };
+    return precios[producto];
+  }
+  
